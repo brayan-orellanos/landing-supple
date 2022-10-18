22a6bd1ff24efc6cffce354a82c82da8b6348a36
brayan.orellanos <brayanx2015@gmail.com>
2022-10-18 11:32:21 -0500

    terminacion de codigo

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 $(function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    $('.btn_indicator').on('click', function () {
+        $(this).tooltip('hide');
+    });
+
     document.addEventListener('click', (e) => {
         if(e.target.matches('.bars_menu') || e.target.matches('.bars_menu *')) {
             document.querySelector('.menu').classList.toggle('header_none')
@@ -44,7 +53,7 @@ $(function () {
                 dots: true,
                 loop:true,
             },
-            801:{
+            769:{
                 items:2,
                 nav:true,
                 dots: true,
@@ -70,16 +79,57 @@ $(function () {
             })
 
             $(this).addClass('active_list')
-            console.log($(this)[0])
             $(`#${$(this).data('form')}`).removeClass('none')
         }
     })
 
-    $('#capture').on('change', function () {
-        if(document.getElementById('capture').files[0]) {
-            $('.name_img').html(document.getElementById('capture').files[0].name)
-        } else {
-            $('.name_img').html('')
+    capture('#capture', '#name_img')
+    capture('#capture2', '#name_img2')
+
+    $('.politic').change(function() {
+        if($(this).prop('checked')) {
+            $("#modal_politics").modal("show");
         }
     })
+
+    cambio()
 })
+
+function cambio() {
+    const $sections = document.querySelectorAll('.scroll[data-scroll-spy]')
+
+    const callback = (entries) => {
+        entries.forEach((entry) => {
+            const id = entry.target.dataset.scrollSpy
+            console.log(entries[entries.length - 1].isIntersecting)
+
+            if(entry.isIntersecting) {
+                document.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.add('menu_active')
+            } 
+             else {
+                document.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.remove('menu_active')
+                if(entries[0].isIntersecting) {
+                    document.querySelector(`a[data-scroll-spy][href="#banner"]`).classList.add('menu_active')
+                }
+            }
+        })
+    }
+
+    const options = {
+        threshold: 0.57,
+    }
+
+    const observer = new IntersectionObserver(callback, options)
+
+    $sections.forEach( el => observer.observe(el))
+}
+
+function capture(capture, name) {
+    $(capture).on('change', function () {
+        if(document.querySelector(capture).files[0]) {
+            $(name).html(document.querySelector(capture).files[0].name)
+        } else {
+            $(name).html('')
+        }
+    })
+}
